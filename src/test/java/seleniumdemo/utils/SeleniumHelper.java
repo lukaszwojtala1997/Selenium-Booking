4e1cce5a7193ccee54e229a647ecb92f86dd8ba8
Łukasz Wojtala <lukaszwojtala1997@gmail.com>
2023-11-09 18:31:42 +0100

    Adding Explicit Wait

---
+++ src/test/java/seleniumdemo/utils/SeleniumHelper.java
@@ -0,0 +1,46 @@
+package seleniumdemo.utils;
+
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.MediaEntityModelProvider;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+public class SeleniumHelper {
+
+    public static void waitForElementToExist(WebDriver webDriver, By locator){
+        WebDriverWait webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+    public static void waitForElementToBeVisible(WebDriver webDriver, WebElement locator){
+        WebDriverWait webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(20));
+        webDriverWait.until(ExpectedConditions.visibilityOf(locator));
+    }
+    public static void waitForElementToBeClickable(WebDriver webDriver, WebElement locator){
+        WebDriverWait webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+    public static void waitForNotEmptyList(WebDriver webDriver, By locator){
+        WebDriverWait webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        webDriverWait.until(browser -> browser.findElements(locator).size() > 0);
+    }
+
+    public static MediaEntityModelProvider getScreenshot(WebDriver driver) throws IOException {
+        String path = takeScreenshot(driver);
+        return MediaEntityBuilder.createScreenCaptureFromPath(path).build();
+    }
+
+    private static String takeScreenshot(WebDriver driver) throws IOException {
+        int randomNumber = (int) (Math.random()*1000);
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File file = screenshot.getScreenshotAs(OutputType.FILE);
+        String path = "src/test/resources/screenshots/screenshot" + randomNumber + ".png";
+        FileUtils.copyFile(file, new File(path));
+        return path;
+    }
+}
